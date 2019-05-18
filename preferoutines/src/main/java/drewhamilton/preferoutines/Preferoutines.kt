a4fd144adc3ce7941f1ca8f73fd8e4a8ee486211
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-05-18 19:10:32 +0200

    Pull suspend logic into common private function

--- preferoutines/src/main/java/drewhamilton/preferoutines/Preferoutines.kt
+++ preferoutines/src/main/java/drewhamilton/preferoutines/Preferoutines.kt
@@ -13,60 +13,62 @@ class Preferoutines(
     private val preferences: SharedPreferences
 ) {
 
-    suspend fun getAll(): Map<String, *> = suspendCoroutine {
-        it.resume(preferences.all)
+    suspend fun getAll(): Map<String, *> = suspendCoroutine { continuation ->
+        continuation.resume(preferences.all)
     }
 
-    suspend fun getString(key: String, defaultValue: String?): String? = suspendCoroutine {
-        it.resume(preferences.getString(key, defaultValue))
-    }
+    suspend fun getString(key: String, defaultValue: String?) =
+        getSuspendedPreference(SharedPreferences::getString, key, defaultValue)
 
     @FlowPreview
     fun getStringFlow(key: String, defaultValue: String?) =
         getPreferenceFlow(SharedPreferences::getString, key, defaultValue)
 
-    suspend fun getStringSet(key: String, defaultValue: Set<String>?): Set<String>? = suspendCoroutine {
-        it.resume(preferences.getStringSet(key, defaultValue))
-    }
+    suspend fun getStringSet(key: String, defaultValue: Set<String>?) =
+        getSuspendedPreference(SharedPreferences::getStringSet, key, defaultValue)
 
     @FlowPreview
     fun getStringSetFlow(key: String, defaultValue: Set<String>?) =
         getPreferenceFlow(SharedPreferences::getStringSet, key, defaultValue)
 
-    suspend fun getInt(key: String, defaultValue: Int): Int = suspendCoroutine {
-        it.resume(preferences.getInt(key, defaultValue))
-    }
+    suspend fun getInt(key: String, defaultValue: Int) =
+        getSuspendedPreference(SharedPreferences::getInt, key, defaultValue)
 
     @FlowPreview
     fun getIntFlow(key: String, defaultValue: Int) =
         getPreferenceFlow(SharedPreferences::getInt, key, defaultValue)
 
-    suspend fun getLong(key: String, defaultValue: Long): Long = suspendCoroutine {
-        it.resume(preferences.getLong(key, defaultValue))
-    }
+    suspend fun getLong(key: String, defaultValue: Long) =
+        getSuspendedPreference(SharedPreferences::getLong, key, defaultValue)
 
     @FlowPreview
     fun getLongFlow(key: String, defaultValue: Long) =
         getPreferenceFlow(SharedPreferences::getLong, key, defaultValue)
 
-    suspend fun getFloat(key: String, defaultValue: Float): Float = suspendCoroutine {
-        it.resume(preferences.getFloat(key, defaultValue))
-    }
+    suspend fun getFloat(key: String, defaultValue: Float) =
+        getSuspendedPreference(SharedPreferences::getFloat, key, defaultValue)
 
     @FlowPreview
     fun getFloatFlow(key: String, defaultValue: Float) =
         getPreferenceFlow(SharedPreferences::getFloat, key, defaultValue)
 
-    suspend fun getBoolean(key: String, defaultValue: Boolean): Boolean = suspendCoroutine {
-        it.resume(preferences.getBoolean(key, defaultValue))
-    }
+    suspend fun getBoolean(key: String, defaultValue: Boolean) =
+        getSuspendedPreference(SharedPreferences::getBoolean, key, defaultValue)
 
     @FlowPreview
     fun getBooleanFlow(key: String, defaultValue: Boolean) =
         getPreferenceFlow(SharedPreferences::getBoolean, key, defaultValue)
 
-    suspend fun contains(key: String): Boolean = suspendCoroutine {
-        it.resume(preferences.contains(key))
+    suspend fun contains(key: String): Boolean = suspendCoroutine { continuation ->
+        continuation.resume(preferences.contains(key))
+    }
+
+    private suspend fun <T> getSuspendedPreference(
+        getPreference: SharedPreferences.(String, T) -> T,
+        key: String,
+        defaultValue: T
+    ): T = suspendCoroutine { continuation ->
+        continuation.resume(preferences.getPreference(key, defaultValue))
     }
 
     @FlowPreview
