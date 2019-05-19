9baf893c6b4fe3bc9a0e0816322a69cb02357de6
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-05-19 21:37:16 +0200

    Apply better button styles

--- app/src/main/java/drewhamilton/preferoutines/example/MainActivity.kt
+++ app/src/main/java/drewhamilton/preferoutines/example/MainActivity.kt
@@ -1,12 +1,25 @@
 package drewhamilton.preferoutines.example
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.edit.editIntegerValue
+import kotlinx.android.synthetic.main.edit.putButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+
+        editIntegerValue.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                putButton.isEnabled = s?.isNotEmpty() ?: false
+            }
+        })
     }
 }
