32ebb0217edb70f34b95ba6c51bf481e7b33d0d0
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-05-19 21:51:12 +0200

    Apply no limits style to MainActivity

--- app/src/main/java/drewhamilton/preferoutines/example/MainActivity.kt
+++ app/src/main/java/drewhamilton/preferoutines/example/MainActivity.kt
@@ -3,14 +3,19 @@ package drewhamilton.preferoutines.example
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import kotlinx.android.synthetic.main.edit.editIntegerValue
 import kotlinx.android.synthetic.main.edit.putButton
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        applyNoLimits()
         setContentView(R.layout.main)
 
         editIntegerValue.addTextChangedListener(object : TextWatcher {
@@ -21,5 +26,23 @@ class MainActivity : AppCompatActivity() {
                 putButton.isEnabled = s?.isNotEmpty() ?: false
             }
         })
+    }
+
+    private fun applyNoLimits() {
+        val window = window
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+        )
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { _, insets ->
+            val contentView = findViewById<View>(android.R.id.content)
+            contentView.setPadding(
+                insets.stableInsetLeft,
+                insets.stableInsetTop,
+                insets.stableInsetRight,
+                insets.stableInsetBottom
+            )
+            insets
+        }
     }
 }
